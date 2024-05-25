746224872cd405dbb48f8b80337faf02bcb42a97
Zakaria Lachkar <lachkar99zakaria@gmail.com>
2024-05-25 14:10:48 +0100

    init

---
+++ Gmail Clone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gmail Clone
+//
+//  Created by Zakaria Lachkar on 25/5/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gmail Clone/Gmail_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gmail_CloneApp.swift
+//  Gmail Clone
+//
+//  Created by Zakaria Lachkar on 25/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct Gmail_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
